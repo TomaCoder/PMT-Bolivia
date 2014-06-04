c9706f2b98f84cdacd86d14ba810dfa6666c3f5c
rrlara <rrlara@gmail.com>
2014-06-04 10:41:47 -0700

    Disabled the string to capitalize the first letter of the string coming from the database.

--- Application/php/getActivityDetailsNew.php
+++ Application/php/getActivityDetailsNew.php
@@ -27,25 +27,8 @@
 
 		$result = pg_query($dbPostgres, $sql) or die("error");
 		      
-		while($rows = pg_fetch_object($result)) {
-			
-			$r2 = array();
-			
-			foreach(json_decode($rows->response) as $key=>$val) {
-			  if (is_array($val))
-			    $r2[$key] = $val;
-			  else if ($key == "title")
-			    $r2[$key] = ucwords(strtolower($val));
-			  else
-			    $r2[$key] = ucfirst(strtolower($val));
-			    
-			}
-			
-			(Object)$r2;
-
-	    }	
-		
-		echo json_encode($r2);
+		$rows = pg_fetch_object($result);
+		echo json_encode(json_decode($rows->response));
 		  
 		} catch(Exception $e) {  
 			header('HTTP/1.1 ' . $e->getCode() . ' ' . $e->getMessage());
